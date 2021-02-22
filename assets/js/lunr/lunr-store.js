17bd1b676d8ec49207048899e93bda115f0f2ddd
Paul Vickers <paul.vickers@northumbria.ac.uk>
2021-02-22 11:43:06 +0000

    Build site at 2021-02-22 11:43:06 UTC

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,6 @@ var store = [{
         "title": "Grant Awarded!",
         "excerpt":"Grant Awarded! The Leverhulme Trust announced its decision to fund this exciting research project titled “A radical new interdisciplinary space for sonification”, or RADICAL for short (grant number RPG-2020-113). Project RADICAL brings together a team of interdisciplinary researchers at Northumbria University and Newcastle University to work on a three-year programme...","categories": ["chat","public"],
         "tags": ["sonification","aesthetics","listening"],
-        "url": "https://projectradical.github.io/chat/public/grant-awarded/",
+        "url": "/chat/public/grant-awarded/",
         "teaser": null
       }]
