e1760c5ce4cdb80fca7db06b0ba4c9c385c1f9ea
Paul Vickers <paul.vickers@northumbria.ac.uk>
2021-11-22 10:39:13 +0000

    Build site at 2021-11-22 10:39:13 UTC

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Speculative Sound Grammars",
-        "excerpt":"A Radical New Interdisciplinary Space for Sonification invites you to join researchers from Newcastle University and Northumbria University Newcastle for an evening of sounds, texts, and noises in the form of performances, talks, spatial audio, and sonifications.     ","categories": [],
+        "excerpt":"A Radical New Interdisciplinary Space for Sonification invites you to join researchers from Newcastle University and Northumbria University Newcastle for an evening of sounds, texts, and noises in the form of performances, talks, spatial audio, and sonifications. Note, due to unforeseen circumstances we have had to postpone the event planned...","categories": [],
         "tags": [],
         "url": "/ssg/",
         "teaser": null
@@ -9,11 +9,5 @@ var store = [{
         "excerpt":"Grant Awarded! The Leverhulme Trust announced its decision to fund this exciting research project titled “A radical new interdisciplinary space for sonification”, or RADICAL for short (grant number RPG-2020-113). Project RADICAL brings together a team of interdisciplinary researchers at Northumbria University and Newcastle University to work on a three-year programme...","categories": ["chat","public"],
         "tags": ["sonification","aesthetics","listening"],
         "url": "/chat/public/grant-awarded/",
-        "teaser": null
-      },{
-        "title": "Speculative Sound Grammars",
-        "excerpt":"A Radical New Interdisciplinary Space for Sonification invites you to join researchers from Newcastle University and Northumbria University Newcastle for an evening of sounds, texts, and noises in the form of performances, talks, spatial audio, and sonifications.     ","categories": [],
-        "tags": [],
-        "url": "/ssg/",
         "teaser": null
       }]
